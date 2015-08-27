8a4aed91565936f176cdf0fcd025c374738a6724
Carlosway89 <carlosway89@gmail.com>
2015-08-27 20:56:11 +0200

    composer integration

--- protected/views/layouts/panel.php
+++ protected/views/layouts/panel.php
@@ -196,7 +196,7 @@
                         
                     </div>
                 </li>
-                <li class="dropdown"><a href="#" data-toggle="dropdown"><i class="fa fa-bell-o"></i><span class="badg">3</span></a>
+              <li class="dropdown"><a href="#" data-toggle="dropdown"><i class="fa fa-bell-o"></i><span class="badg">3</span></a>
                 	<div class="dropdown-menu arrow pull-right md panel panel-default arrow-top-right notifications">
                         <div class="panel-heading">
                       	Notification
@@ -257,33 +257,25 @@
                         </div>
                         
                     </div>
-                </li>
-                <li class="dropdown"><a href="#" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></a>
+              </li>
+              <li class="dropdown"><a href="#" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></a>
                 	<div class="dropdown-menu lg pull-right arrow panel panel-default arrow-top-right">
                     	<div class="panel-heading">
-                        	More Apps
+                        	Panel Options
                         </div>
                         <div class="panel-body text-center">
                         	<div class="row">
                             	<div class="col-xs-6 col-sm-4"><a href="#" class="text-green"><span class="h2"><i class="fa fa-envelope-o"></i></span><p class="text-gray no-margn">Messages</p></a></div>
-                                <div class="col-xs-6 col-sm-4"><a href="#" class="text-purple"><span class="h2"><i class="fa fa-calendar-o"></i></span><p class="text-gray no-margn">Events</p></a></div>
+                                <div class="col-xs-6 col-sm-4"><a href="#" class="text-purple"><span class="h2"><i class="fa fa-calendar-o"></i></span><p class="text-gray no-margn">Page Config</p></a></div>
                                 
                                 <div class="col-xs-12 visible-xs-block"><hr></div>
                                 
-                                <div class="col-xs-6 col-sm-4"><a href="#" class="text-red"><span class="h2"><i class="fa fa-comments-o"></i></span><p class="text-gray no-margn">Chatting</p></a></div>
-                                
-                                <div class="col-lg-12 col-md-12 col-sm-12  hidden-xs"><hr></div>
-                            
-                            	<div class="col-xs-6 col-sm-4"><a href="#" class="text-yellow"><span class="h2"><i class="fa fa-folder-open-o"></i></span><p class="text-gray">Folders</p></a></div>
-                                
-                                <div class="col-xs-12 visible-xs-block"><hr></div>
-                                
-                                <div class="col-xs-6 col-sm-4"><a href="#" class="text-primary"><span class="h2"><i class="fa fa-flag-o"></i></span><p class="text-gray">Task</p></a></div>
-                                <div class="col-xs-6 col-sm-4"><a href="#" class="text-info"><span class="h2"><i class="fa fa-star-o"></i></span><p class="text-gray">Favorites</p></a></div>
-                            </div>
+                                <div class="col-xs-6 col-sm-4"><a href="<?=Yii::app()->request->baseUrl?>/site/logout" class="text-red"><span class="h2"><i class="fa fa-sign-out"></i></span><p class="text-gray no-margn">Log Out</p></a></div>
+                                                      
+                          </div>
                         </div>
                     </div>
-                </li>
+              </li>
             </ul>
         </header>
         <!-- Header Ends -->
