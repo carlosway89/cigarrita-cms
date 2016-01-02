b145be3ad027fb3243c50dcd30ac16d9408f707a
Carlosway89 <carlosway89@gmail.com>
2016-01-02 12:43:24 +0100

    cleaning up

--- protected/config/console.php
+++ protected/config/console.php
@@ -63,7 +63,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=ikt',
+			'connectionString' => 'mysql:host=localhost;dbname=cigarritaworker_v2',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '150189',
--- protected/config/main.php
+++ protected/config/main.php
@@ -14,24 +14,12 @@ return array(
 	'theme'=>'design',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	// 'preload'=>array('log'),
 
 	// autoloading model and component classes
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
-	),
-
-	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'150189',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-		
 	),
 
 	// application components
@@ -47,17 +35,6 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
-		// uncomment the following to enable URLs in path-format
-		// 'urlManager'=>array(
-		// 	'urlFormat'=>'path',
-		// 	'showScriptName'=>false,
-		// 	// 'urlSuffix'=>'.html',
-		// 	'rules'=>array(
-		// 		'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-		// 		'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-		// 		'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-		// 	),
-		// ),
 		'urlManager'=>array(
 		    'urlFormat'=>'path',
 		    'showScriptName'=>false,
@@ -79,14 +56,7 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 
 			),
-		),
-		// 'db'=>array(
-		// 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		// ),
-		// uncomment the following to use a MySQL database
-		
-		
-		
+		),			
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
@@ -98,12 +68,6 @@ return array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
 				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
 			),
 		),
 		'authManager'=>array(
@@ -111,11 +75,7 @@ return array(
 			'connectionID'=>'db',
 		),
 	),
-
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
 	'params'=>array(
-		// this is used in contact page
 		'adminEmail'=>'carlos@cigarrita-worker.com',
 	),
 );
