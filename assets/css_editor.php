0d82696f3d448507bb1cbfafd66cf5bd667ecf76
Carlos Manay <carlosway89@gmail.com>
2016-04-14 15:58:11 +0200

    editor css
    cleaning up

--- assets/css_editor.php
+++ assets/css_editor.php
@@ -7,23 +7,7 @@
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 
 <!--[inline-editor] -->
-<!--
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/froala_editor.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/froala_style.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/code_view.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/colors.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/emoticons.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/image_manager.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/image.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/line_breaker.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/table.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/file.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/char_counter.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/video.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/emoticons.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/inline_editor/css/plugins/fullscreen.css">
-<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/assets/editor/css/codemirror.min.css">
--->
+
 <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/min/?g=inlineEditorCSS">
 <!-- [end] -->
 
