b169bea4593e2fa234b9b62f3facd72eafc9c4f3
Carlosway89 <carlosway89@gmail.com>
2016-05-19 20:05:26 +0200

    improvements

---
+++ _min/groupsConfig.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
+
+/**
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ *
+ * See https://github.com/mrclay/minify/blob/master/docs/CustomServer.wiki.md for other ideas
+ **/
+
+return array(
+	"inlineEditorJs"=>array(
+		"//assets/editor/js/plugins/codemirror.min.js",
+		"//assets/editor/js/plugins/xml.min.js",
+		"//assets/editor/inline_editor/js/froala_editor.min.js",
+		"//assets/editor/inline_editor/js/plugins/align.min.js",
+		"//assets/editor/inline_editor/js/plugins/code_beautifier.min.js",
+		"//assets/editor/inline_editor/js/plugins/code_view.min.js",
+		"//assets/editor/inline_editor/js/plugins/colors.min.js",
+		"//assets/editor/inline_editor/js/plugins/emoticons.min.js",
+		"//assets/editor/inline_editor/js/plugins/font_size.min.js",
+		"//assets/editor/inline_editor/js/plugins/font_family.min.js",
+		"//assets/editor/inline_editor/js/plugins/image.min.js",
+		"//assets/editor/inline_editor/js/plugins/image_manager.min.js",
+		"//assets/editor/inline_editor/js/plugins/line_breaker.min.js",
+		"//assets/editor/inline_editor/js/plugins/link.min.js",
+		"//assets/editor/inline_editor/js/plugins/lists.min.js",
+		"//assets/editor/inline_editor/js/plugins/paragraph_format.min.js",
+		"//assets/editor/inline_editor/js/plugins/paragraph_style.min.js",
+		"//assets/editor/inline_editor/js/plugins/video.min.js",
+		"//assets/editor/inline_editor/js/plugins/table.min.js",
+		"//assets/editor/inline_editor/js/plugins/url.min.js",
+		"//assets/editor/inline_editor/js/plugins/emoticons.min.js",
+		"//assets/editor/inline_editor/js/plugins/file.min.js",
+		"//assets/editor/inline_editor/js/plugins/entities.min.js",
+		"//assets/editor/inline_editor/js/plugins/char_counter.min.js",
+		"//assets/editor/inline_editor/js/plugins/inline_style.min.js",
+		"//assets/editor/inline_editor/js/plugins/save.min.js",
+		"//assets/editor/inline_editor/js/plugins/fullscreen.min.js",
+	),
+	"inlineEditorCSS"=>array(
+		"//assets/editor/inline_editor/css/froala_editor.css",
+		"//assets/editor/inline_editor/css/froala_style.css",
+		"//assets/editor/inline_editor/css/plugins/code_view.css",
+		"//assets/editor/inline_editor/css/plugins/colors.css",
+		"//assets/editor/inline_editor/css/plugins/emoticons.css",
+		"//assets/editor/inline_editor/css/plugins/image_manager.css",
+		"//assets/editor/inline_editor/css/plugins/image.css",
+		"//assets/editor/inline_editor/css/plugins/line_breaker.css",
+		"//assets/editor/inline_editor/css/plugins/table.css",
+		"//assets/editor/inline_editor/css/plugins/file.css",
+		"//assets/editor/inline_editor/css/plugins/char_counter.css",
+		"//assets/editor/inline_editor/css/plugins/video.css",
+		"//assets/editor/inline_editor/css/plugins/emoticons.css",
+		"//assets/editor/inline_editor/css/plugins/fullscreen.css",
+		"//assets/editor/css/codemirror.min.css",
+	),
+	"angularJs"=>array(
+		"//assets/editor/js/plugins/angular.min.js",
+		"//assets/editor/js/plugins/angular-animate.min.js",
+		"//assets/editor/js/plugins/angular-route.min.js",
+		"//assets/editor/js/plugins/angular-resource.min.js",
+	),	
+	"cigarritaApp"=>array(
+		"//assets/editor/js/controllers/controllers.js",
+		"//assets/editor/js/modules/animations.js",
+		"//assets/editor/js/modules/filters.js",
+		"//assets/editor/js/modules/directives.js",
+		"//assets/editor/js/models/services.js",
+	),
+	"cigarritaAppWeb"=>array(
+		"//assets/web/js/controllers/controllers.js",
+		"//assets/web/js/modules/animations.js",
+		"//assets/web/js/modules/filters.js",
+		"//assets/web/js/modules/directives.js",
+		"//assets/web/js/models/services.js",
+	),
+);
